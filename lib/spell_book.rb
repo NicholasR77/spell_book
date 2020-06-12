0598e575dd5206c6ceba5a77fd7e536c91ec4d66
NicholasR77 <nl7robbins@icloud.com>
2020-06-12 00:12:37 -0700

    Finished a basic MVP of the app which includes multiple methods for searching class spells

--- lib/spell_book.rb
+++ lib/spell_book.rb
@@ -1,16 +1,21 @@
 class SpellBook
   attr_accessor :cli
 
+  def initialize
+    self.cli = CLI.new
+    self.welcome_user
+  end
+
   def welcome_user
     self.cli.print_welcome
-    sleep(2)
+    sleep(3)
     self.cli.print_class_choice_list
   end
 
   def find_class_by_name
     self.cli.print_choose_class
     dnd_class_input = self.cli.gets_user_input
-    found_dnd_class = DnDClass.all.find {|dnd_class| dnd_class.name == dnd_class_input}
+    found_dnd_class = DnDClass.all.find {|dnd_class| dnd_class.name.downcase == dnd_class_input}
 
     if (found_dnd_class == nil)
       self.cli.print_enter_valid_input
@@ -21,35 +26,119 @@ class SpellBook
   end
 
   def find_class_spells_by_name(dnd_class_input)
+
     self.cli.print_choose_spell_name
+
     dnd_spell_input = self.cli.gets_user_input
-    found_dnd_spell = dnd_class_input.sb_dnd_spell.find{|dnd_spell| dnd_spell.name == dnd_spell_input}
-    
-    if (found_dnd_spell == nil)
-      self.cli.print_enter_valid_input
-      self.find_class_spells_by_name
+    found_dnd_spells = []
+
+    if ((dnd_class_input.sb_dnd_spell.any?{|dnd_spell| dnd_spell.name.downcase == dnd_spell_input} == true))
+      found_dnd_spells << dnd_class_input.sb_dnd_spell.find {|dnd_spell| dnd_spell.name.downcase == dnd_spell_input}
     else
-      found_dnd_spell
+      dnd_spell_input_split = dnd_spell_input.split(" ")
+  
+      dnd_spell_input_split.each do |dnd_spell_input| 
+        dnd_class_input.sb_dnd_spell.each do |dnd_spell|
+          if (dnd_spell.slug.include?(dnd_spell_input) == true)
+            found_dnd_spells << dnd_spell
+          end
+        end
+      end
+    end
+    
+    if (found_dnd_spells.length <= 0)
+      self.cli.print_not_able_to_find_spell 
+      self.find_class_spells_by_name(dnd_class_input)
+    else
+      found_dnd_spells
     end
   end
 
-  def self.start
-    new_spellbook = SpellBook.new
-    new_spellbook.cli = CLI.new
-    new_spellbook.welcome_user
-    dnd_class_input = new_spellbook.find_class_by_name
-    dnd_spell_input = new_spellbook.find_class_spells_by_name(dnd_class_input)
-    new_spellbook.cli.print_spell_info(dnd_spell_input)
+  def find_class_spells_by_level(dnd_class_input)
+
+    self.cli.print_choose_spell_level
+    dnd_spell_input_split = self.cli.gets_user_input.split(", ").collect {|split_str| split_str.to_i}
+
+    if (dnd_spell_input_split.any? {|split_int| split_int < 0 || split_int > 9} == true)
+      self.cli.print_enter_valid_spell_level 
+      self.find_class_spells_by_level(dnd_class_input)
+    else  
+      found_dnd_spells = []
+  
+      dnd_spell_input_split.each do |dnd_spell_input| 
+        dnd_class_input.sb_dnd_spell.each do |dnd_spell|
+          if (dnd_spell.level_int == dnd_spell_input)
+            found_dnd_spells << dnd_spell
+          end
+        end
+      end
+
+      if (found_dnd_spells.length <= 0)
+        self.cli.print_not_able_to_find_spell 
+        self.find_class_spells_by_level(dnd_class_input)
+      else
+        found_dnd_spells.uniq
+      end
+    end
+    
+  end
+
+  def find_class_spells_by_school(dnd_class_input)
+
+    self.cli.print_choose_spell_school
+
+    dnd_spell_input = self.cli.gets_user_input
+    found_dnd_spells = []
+
+    if ((dnd_class_input.sb_dnd_spell.any?{|dnd_spell| dnd_spell.school.downcase == dnd_spell_input} == true))
+      found_dnd_spells << dnd_class_input.sb_dnd_spell.find {|dnd_spell| dnd_spell.school.downcase == dnd_spell_input}
+    else
+      dnd_spell_input_split = dnd_spell_input.split(" ")
+  
+      dnd_spell_input_split.each do |dnd_spell_input| 
+        dnd_class_input.sb_dnd_spell.each do |dnd_spell|
+          if (dnd_spell.school.include?(dnd_spell_input) == true)
+            found_dnd_spells << dnd_spell
+          end
+        end
+      end
+    end
+    
+    if (found_dnd_spells.length <= 0)
+      self.cli.print_not_able_to_find_spell 
+      self.find_class_spells_by_school(dnd_class_input)
+    else
+      found_dnd_spells
+    end
+  end
+
+  def start_search
+    dnd_class_input = self.find_class_by_name
+
+    search_method_input = 0
+
+    until (search_method_input == 4 || search_method_input == 5 )
+      self.cli.print_choose_search_method
+      search_method_input = self.cli.gets_user_input.to_i
+
+      if (search_method_input == 1)
+        dnd_spell_input = self.find_class_spells_by_name(dnd_class_input)
+        self.cli.print_spell_list(dnd_spell_input)
+      elsif (search_method_input == 2)  
+        dnd_spell_input = self.find_class_spells_by_level(dnd_class_input)
+        self.cli.print_spell_list(dnd_spell_input)
+      elsif (search_method_input == 3)  
+        dnd_spell_input = self.find_class_spells_by_school(dnd_class_input)
+        self.cli.print_spell_list(dnd_spell_input)
+      elsif (search_method_input == 4)
+        self.start_search
+      elsif (search_method_input == 5)
+        exit
+      else
+        self.start_search
+        self.cli.print_enter_valid_input
+      end
+    end
+
   end
 end
-
-#API.start_object_creation
-
-#test1 = DnDSpell.all.find{|dnd_spell| dnd_spell.name == "Acid Arrow"}
-#test1.sb_dnd_class.count
-#test2 = DnDClass.all.find{|dnd_class| dnd_class.name == "Druid"}
-#test2.sb_dnd_spell
-#test2.sb_dnd_spell.count
-#test3 = DnDClass.all.find{|dnd_class| dnd_class.name == "Wizard"}
-#test3.sb_dnd_spell
-#test3.sb_dnd_spell.count
--- lib/spell_book/cli.rb
+++ lib/spell_book/cli.rb
@@ -6,57 +6,90 @@ class CLI
         self.print_row
         puts "Welcome to the class SpellBook tool using the D&D 5e SRD API."
         puts "This tool allows you to find the spells for any D&D 5e Class."
-        self.print_row
     end
     def print_class_choice_list
+        self.print_row
         puts "Here are the classes available to search from:"
         DnDClass.all.each do |dnd_class|
             puts "#{dnd_class.name}"
         end
-        self.print_row
     end
     def print_choose_class
-        puts "Please enter the exact name of the class of which you want to view spells, such Barbarian or Cleric. Capitilzation does not matter."
         self.print_row
+        puts "Please enter the exact name of the class of which you want to view spells, such Barbarian or Cleric. Capitilzation does not matter."
+    end
+
+    def print_choose_search_method
+        self.print_row
+        puts "Please enter the number for how you want to proceed:"
+        puts "1: Search spells by name"
+        puts "2: Search spells by level"
+        puts "3: Search spells by school of magic"
+        puts "4: To search using another class"
+        puts "5: To exit the program"
     end
 
     def print_choose_spell_name
-        puts "If you know the name of the spell, please enter it. Capitilzation does not matter."
         self.print_row
+        puts "If you know the name of the spell, such as Fireball or Bane, please enter it. Otherwise, the search will return spells most applicable to the submitted query. Capitilzation does not matter."
+    end
+
+    def print_choose_spell_level
+        self.print_row
+        puts "Please enter the spell level you want to view as 0 through 9, 0 being cantrips. If you want to view mutliple spell levels, please enter each spell level followed by a comma, such as: 0, 1, 2."
+    end
+
+    def print_choose_spell_school
+        self.print_row
+        puts "If you know the name of the school of magic, such as Conjuration or Illusion, please enter it. Otherwise, the search will return spells most applicable to the submitted query. Capitilzation does not matter."
     end
 
     def print_enter_valid_input
-        puts "Please enter a valid input."
         self.print_row
+        puts "Please enter a valid input."
+    end
+
+    def print_not_able_to_find_spell
+        self.print_row
+        puts "Spell wasn't found. Please try again."
+    end
+
+    def print_enter_valid_spell_level
+        self.print_row
+        puts "Please enter a valid spell level of 0 through 9, 0 being cantrips. If you want to view mutliple spell levels, please enter each spell level followed by a comma, such as: 0, 1, 2."
     end
 
     def print_loading_spells_message
-        puts "One moment as we load your spells...."
         self.print_row
+        puts "One moment as we load your spells...."
     end
 
-    def print_spell_info(dnd_spell_input)
+    def print_spell_list(dnd_spell_input_arr)
         self.print_row
-        puts "Name: #{dnd_spell_input.name}"
-        puts "Description: #{dnd_spell_input.desc}"
-        puts "Casting at Higher Levels: #{dnd_spell_input.higher_level}"
-        puts "Physical Book with Page: #{dnd_spell_input.page.upcase}"
-        puts "Range: #{dnd_spell_input.range}"
-        puts "Components: #{dnd_spell_input.components}"
-        puts "Materials: #{dnd_spell_input.material}"
-        puts "Ritual: #{dnd_spell_input.ritual.capitalize}"
-        puts "Duration: #{dnd_spell_input.duration}"
-        puts "Concentration: #{dnd_spell_input.concentration.capitalize}"
-        puts "Casting Time: #{dnd_spell_input.casting_time}"
-        puts "Base Level: #{dnd_spell_input.level}"
-        puts "School of Magic: #{dnd_spell_input.school}"
-        puts "Classes Available for Use: #{dnd_spell_input.dnd_class}"
-        puts "Archetype Available for Use: #{dnd_spell_input.archetype}"
-        puts "Circles Available for Use: #{dnd_spell_input.circles}"
-        self.print_row
+        puts "Here is a list of spells that fit that search:"
+        dnd_spell_input_arr.each do |dnd_spell_input|
+            self.print_row
+            puts "Name: #{dnd_spell_input.name}"
+            puts "Description: #{dnd_spell_input.desc}"
+            puts "Casting at Higher Levels: #{dnd_spell_input.higher_level}"
+            puts "Physical Book with Page: #{dnd_spell_input.page.upcase}"
+            puts "Range: #{dnd_spell_input.range}"
+            puts "Components: #{dnd_spell_input.components}"
+            puts "Materials: #{dnd_spell_input.material}"
+            puts "Ritual: #{dnd_spell_input.ritual.capitalize}"
+            puts "Duration: #{dnd_spell_input.duration}"
+            puts "Concentration: #{dnd_spell_input.concentration.capitalize}"
+            puts "Casting Time: #{dnd_spell_input.casting_time}"
+            puts "Base Level: #{dnd_spell_input.level}"
+            puts "School of Magic: #{dnd_spell_input.school}"
+            puts "Classes Available for Use: #{dnd_spell_input.dnd_class}"
+            puts "Archetype Available for Use: #{dnd_spell_input.archetype}"
+            puts "Circles Available for Use: #{dnd_spell_input.circles}"
+        end
     end
 
     def gets_user_input
-        gets.strip.downcase.capitalize
+        self.print_row
+        gets.strip.downcase
     end
 end
